f4c08e4fd1f5567772d9efb35fb4c69146d4be14
MarkByh <123231312fh@gmail.com>
2023-05-29 13:46:55 +0300

    f

--- src/redux/contactsSllice/operations.js
+++ src/redux/contactsSllice/operations.js
@@ -6,36 +6,36 @@ axios.defaults.baseURL =
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchContacts',
-    async () => {
+    async (_, thunkAPI) => {
         try {
             const response = await axios.get('/contacts');
             return response.data;
         } catch (error) {
-            return error;
+            return thunkAPI.rejectWithValue('Error, somthing vent wrong');
         }
     }
 );
 
 export const deleteContact = createAsyncThunk(
     'contacts/deleteContact',
-    async (id) => {
+    async (id, thunkAPI) => {
         try {
             const response = await axios.delete(`/contacts/${id}`);
             return response.data;
         } catch (error) {
-            return error;
+            return thunkAPI.rejectWithValue('Error! somthing vent wrong');
         }
     }
 );
 
 export const addContact = createAsyncThunk(
     'contacts/addContact',
-    async (contact) => {
+    async ({ name, number }, thunkAPI) => {
         try {
-            const response = await axios.post('/contacts', { ...contact });
+            const response = await axios.post('/contacts', { name, number });
             return response.data;
         } catch (error) {
-            return error;
+            return thunkAPI.rejectWithValue('Error! somthing vent wrong');
         }
     }
 );
